112e8eecc2c0c1bffd9fd9f29934e490e061a679
TA-Sakin <tasakin98@gmail.com>
2022-04-18 13:03:23 +0600

    about me

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div style={{ minHeight: "70vh" }}>
+      <h5 className="w-50 mx-auto text-center py-5">
+        Hello, I'm Sakin currently learning web development. Till now I have
+        created various front-end application using HTML, CSS, JavaScript, React
+        and Firebase. So far the learning process has been quite fascinating. I
+        aspire to become a full-stack web developer within next 1 year.
+      </h5>
+    </div>
+  );
 };
 
 export default About;
--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Blogs = () => {
-  return <div></div>;
+  return (
+    <div style={{ minHeight: "70vh" }} className="my-5 container">
+      <h3>1. Difference between authorization and authentication</h3>
+      <p className="pt-2 pb-4 fs-3">
+        Authentication is the process of verrifying who the person is and
+        authorization is the process of verifying what the person has access to.
+        For example, the process of verifying and confirming users ID and
+        passwords for an application is called authentication, but determining
+        which files or data user has access to is called authorization.
+      </p>
+      <h3>1. Difference between authorization and authentication</h3>
+      <p className="pt-2 pb-4 fs-3">
+        Authentication is the process of verrifying who the person is and
+        authorization is the process of verifying what the person has access to.
+        For example, the process of verifying and confirming users ID and
+        passwords for an application is called authentication, but determining
+        which files or data user has access to is called authorization.
+      </p>
+      <h3>1. Difference between authorization and authentication</h3>
+      <p className="pt-2 pb-4 fs-3">
+        Authentication is the process of verrifying who the person is and
+        authorization is the process of verifying what the person has access to.
+        For example, the process of verifying and confirming users ID and
+        passwords for an application is called authentication, but determining
+        which files or data user has access to is called authorization.
+      </p>
+    </div>
+  );
 };
 
 export default Blogs;
