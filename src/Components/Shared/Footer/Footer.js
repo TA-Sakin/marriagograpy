13a4909b01c4d9c6e1c7ab93235bb1d4ae514fa3
TA-Sakin <tasakin98@gmail.com>
2022-04-18 23:39:22 +0600

    footer icon added

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Footer.css";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaPinterest,
+} from "react-icons/fa";
 const Footer = () => {
   let year = new Date();
   return (
     <div className="text-center w-100 mt-5 py-5 bg-dark text-secondary h-100 footer">
       <p>Marriagograpy &copy; {year.getFullYear()}. All rights reserved.</p>
+      <div className="d-flex justify-content-center fs-5">
+        <FaFacebook className="mx-2" />
+        <FaInstagram className="mx-2" />
+        <FaYoutube className="mx-2" />
+        <FaTwitter className="mx-2" />
+        <FaPinterest className="mx-2" />
+      </div>
     </div>
   );
 };
